eb334b22403bee66f576204150c96242d9cd4542
EHinrich <90458838+EHinrich@users.noreply.github.com>
2022-05-05 17:56:48 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -82,6 +82,33 @@ $db = new PDO('mysql:host=localhost;dbname=u41181', $user, $password, array(PDO:
                                     <td><?php print $values['pass'] ?></td>
                                     <td><?php print '<a href="form.php">Редактировать</a>' ?></td>
                                     <td><?php print '<a href="form.php">Удалить</a>' ?></td>
+                                    <?php 
+                                    $user = 'u41181';
+                                    $password = '2342349';
+                                    $db = new PDO('mysql:host=localhost;dbname=u41181', $user, $password, array(PDO::ATTR_PERSISTENT => true));
+
+                                      try {
+                                          $id =$values['id'];
+                                        $stmt = $db->prepare("SELECT * FROM form2 WHERE id = $id");
+                                        $stmt->execute();
+                                        foreach ($stmt as $row) {
+                                          $values['name']=$row["name"];
+                                          $values['email'] = $row["email"];
+                                          $values['year'] = $row["year"];
+                                          $values['sex'] = $row["sex"];
+                                          $values['number_of_limbs'] = $row["number_of_limbs"];
+                                          $values['superpowers'] = $row["superpowers"];
+                                          $values['biography'] = $row["biography"];
+                                          $values['checkbox'] = $row["checkbox"];
+                                          }
+
+                                        }
+                                          catch(PDOException $e){
+                                            print('Error : ' . $e->getMessage());
+                                            exit();
+                                       }
+                                include('form.php');
+                                      ?>
                                 </tr>
                                 <?php
                         
